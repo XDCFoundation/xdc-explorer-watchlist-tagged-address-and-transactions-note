82838e68e1f9d9becf3a29b2886afcd2f9ff988b
tushar <tushar@leewayhertz.com>
2021-10-20 17:17:58 +0530

    change requestion data from body to params

--- app/modules/transactionPrivateNote/index.js
+++ app/modules/transactionPrivateNote/index.js
@@ -5,7 +5,7 @@ import BLManager from './manger'
 export default class Index {
   async transactionPrivateNote (request, response) {
     // lhtWebLog('Inside testRoute', request.body, 'testRoute', 0, '')
-    const [error, getMetersRes] = await Utils.parseResponse(new BLManager().fetchtransactionPrivateNote(request.body))
+    const [error, getMetersRes] = await Utils.parseResponse(new BLManager().fetchtransactionPrivateNote(request.params))
     if (!getMetersRes) { return Utils.handleError(error, request, response) }
     return Utils.response(response, getMetersRes, apiSuccessMessage.FETCH_SUCCESS, httpConstants.RESPONSE_STATUS.SUCCESS, httpConstants.RESPONSE_CODES.OK)
   }
