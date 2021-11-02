95de52d41ac08efe82d2ecdf8b98e3821a590ca6
tushar <tushar@leewayhertz.com>
2021-11-02 17:34:59 +0530

    some changes

--- routes/index.js
+++ routes/index.js
@@ -13,65 +13,32 @@ module.exports = (app) => {
   /**
    * route definition
    */
+
   // app.get("/test-route", ValidationManger.validateUserLogin, new TestModule().testRoute);
-  app.get(
-    "/transaction-private-note/:userId",
-    new TrxPvtModule().transactionPrivateNote
-  );
+  app.get("/transaction-private-note/:userId",new TrxPvtModule().transactionPrivateNote);
 
-  app.post(
-    "/add-transaction-label",
-    ValidationManger.validateTransactionLable,
-    new AddTransactionLabel().addTransactionLabel
-  );
+  app.post("/add-transaction-label",ValidationManger.validateTransactionLable,new AddTransactionLabel().addTransactionLabel);
 
-  app.post(
-    "/add-address-tag",
-    ValidationManger.validateTagAddressLable,
-    new TaggedAddress().addTagAddress
-  );
+  app.post("/add-address-tag",ValidationManger.validateTagAddressLable,new TaggedAddress().addTagAddress);
 
   app.get("/get-address-tag/:userId", new TaggedAddress().getTagAddress);
 
-  app.post(
-    "/add-watchlist",
-    ValidationManger.validateWatchList,
-    new AddWatchList().addWatchList
-  );
+  app.post("/add-watchlist", ValidationManger.validateWatchList, new AddWatchList().addWatchList );
 
   app.get("/getAddress/:userId", new AddWatchList().getAddressByUserId);
 
-  app.put(
-    "/edit-address-tag",
-    ValidationManger.validateEditTagAddressLable,
-    new TaggedAddress().editTagAddress
-  );
+  app.put("/edit-address-tag", ValidationManger.validateEditTagAddressLable, new TaggedAddress().editTagAddress );
 
-  app.put(
-    "/edit-watchlist",
-    ValidationManger.validateEditWatchList,
-    new AddWatchList().editWatchList
-  );
+  app.put("/edit-watchlist",ValidationManger.validateEditWatchList, new AddWatchList().editWatchList );
 
-  app.put(
-    "/edit-transaction-Private-note",
-    ValidationManger.validateEditTransactionPrivateNote,
-    new TrxPvtModule().editTransactionPrivateNote
-  );
+  app.put("/edit-transaction-Private-note",ValidationManger.validateEditTransactionPrivateNote, new TrxPvtModule().editTransactionPrivateNote);
 
   app.post("/get-content-watchlist", new AddWatchList().getContentWatchlist);
 
-  app.post(
-    "/get-content-txn-label",
-    new AddTransactionLabel().getContentTxnLabel
-  );
+  app.post("/get-content-txn-label",new AddTransactionLabel().getContentTxnLabel);
 
-  app.post(
-    "/get-content-tag-address",
-    new TaggedAddress().getContentTagAddress
-  );
-  app.post(
-    "/search",
-    new Serach().searchData
-  );
+  app.post("/get-content-tag-address",new TaggedAddress().getContentTagAddress);
+  
+  app.post("/search",new Serach().searchData );
+
 };
