1677a17d12be0ff0e9f8a7211379f1bf83f555bb
tushar <tushar@leewayhertz.com>
2021-11-03 16:23:03 +0530

    Merged 2 schema in 1 and implemented search api

--- app/modules/search/searchManager.js
+++ app/modules/search/searchManager.js
@@ -1,55 +1,63 @@
-import { apiFailureMessage, httpConstants} from "../../common/constants";
+import { apiFailureMessage, httpConstants } from "../../common/constants";
 import Utils from "../../utils";
-import UserTransactionSchema from "../../models/userTransaction"
-import TagAddressSchema from "../../models/tagAddress";
-import UserAddressSchema from "../../models/userAddressModel";
+import UserTransactionSchema from "../../models/UserTransaction";
+import TagAddressSchema from "../../models/UserWhitelistAddress";
+import UserAddressSchema from "../../models/UserWhitelistAddress";
 import Utility from "../../utils";
 export default class Manger {
+  search = async (requestData) => {
+    console.log(requestData);
+    if (!requestData || !requestData.userId)
+      throw Utils.error(
+        {},
+        apiFailureMessage.INVALID_PARAMS,
+        httpConstants.RESPONSE_CODES.FORBIDDEN
+      );
 
-    search = async (requestData) => {
-        console.log(requestData)
-        if (!requestData || !requestData.userId )
+    try {
+      const searchTable = requestData.search;
+      delete requestData.search;
 
-            throw Utils.error({}, apiFailureMessage.INVALID_PARAMS, httpConstants.RESPONSE_CODES.FORBIDDEN);
+      if (search == 0) {
+        let contentRequest = Utility.parseGetContentRequest(requestData);
+        const contentList = await UserAddressSchema.getFilteredData(
+          contentRequest.requestData,
+          contentRequest.selectionKeys,
+          contentRequest.skip,
+          contentRequest.limit,
+          contentRequest.sortingKey
+        );
 
-        try {
+        return contentList;
+      }
 
-            const search =requestData.search
-            delete requestData.search
+      if (search == 1) {
+        let contentRequest = Utility.parseGetContentRequest(requestData);
+        const contentList = await UserTransactionSchema.getFilteredData(
+          contentRequest.requestData,
+          contentRequest.selectionKeys,
+          contentRequest.skip,
+          contentRequest.limit,
+          contentRequest.sortingKey
+        );
 
-            if(search==1)
-            {
-                
+        return contentList;
+      }
 
-                let contentRequest = Utility.parseGetContentRequest(requestData)
-                const contentList = await UserAddressSchema.getFilteredData(contentRequest.requestData, contentRequest.selectionKeys, contentRequest.skip, contentRequest.limit, contentRequest.sortingKey);
-                // console.log("request 1",contentRequest)
-                return contentList;
-            }
+      if (search == 2) {
+        let contentRequest = Utility.parseGetContentRequest(requestData);
+        const contentList = await TagAddressSchema.getFilteredData(
+          contentRequest.requestData,
+          contentRequest.selectionKeys,
+          contentRequest.skip,
+          contentRequest.limit,
+          contentRequest.sortingKey
+        );
 
-            if(search==2)
-            {
-                // delete requestData.search
-
-                let contentRequest = Utility.parseGetContentRequest(requestData)
-                const contentList = await UserTransactionSchema.getFilteredData(contentRequest.requestData, contentRequest.selectionKeys, contentRequest.skip, contentRequest.limit, contentRequest.sortingKey);
-                // console.log("request 2",contentRequest)
-                return contentList;
-            }
-
-            if (search==3)
-            {
-                // delete requestData.search
-                let contentRequest = Utility.parseGetContentRequest(requestData)
-                const contentList = await TagAddressSchema.getFilteredData(contentRequest.requestData, contentRequest.selectionKeys, contentRequest.skip, contentRequest.limit, contentRequest.sortingKey);
-                // console.log("request 3",contentRequest)
-                return contentList;
-
-            }
-
-        }catch (error) {
-            throw error
-        }
+        return contentList;
+      }
+    } catch (error) {
+      throw error;
     }
+  };
 }
-
